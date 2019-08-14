1a76f6a298cc5743cf80f1a505c07b5dcfeac818
micaellajimini <jimin7971@gmail.com>
2019-08-14 21:07:25 +0900

    n & m no.5 no.6

---
+++ 15654.cpp
@@ -0,0 +1,53 @@
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+int N,M;
+vector<int> v;
+vector<int> result;
+int cnt=0;
+int check[10001]={0};
+
+void recur(){
+    if(cnt == M){
+        for(int i=0;i<result.size();i++) printf("%d ",result[i]);
+        printf("\n");
+    }
+    else{
+        for(int i=0;i<v.size();i++){
+            if(!check[v[i]]){
+                result.push_back(v[i]);
+                check[v[i]] = 1;
+                cnt++;
+                recur();
+                cnt--;
+                check[v[i]] = 0;
+                result.pop_back();
+            }
+        }
+    }
+}
+int main(){
+    scanf("%d %d",&N,&M);
+    int temp;
+    scanf("%d",&temp);
+    v.push_back(temp);
+    for(int i=1;i<N;i++){
+        scanf("%d",&temp);
+        if(temp > v[v.size()-1]) v.push_back(temp);
+        else if(temp < v[0]) v.insert(v.begin(),temp);
+        else{
+            for(int j=0;j<v.size()-1;j++){
+                if(v[j] < temp && temp < v[j+1]){
+                    v.insert(v.begin()+j+1,temp);
+                    break;
+                }
+            }
+        }
+    }
+    //for(int i=0;i<v.size();i++) printf("%d ",v[i]);
+    recur();
+    return 0;
+}
+
